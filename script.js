47993342c91b7b3f59b5a951446ee9f6b1284cbc
Mike <mikikoman@gamil.com>
2016-07-04 17:51:41 +0200

    fix&fix

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-$(function(){
-setInterval(swing, 100);
-carouselList.animate({marginLeft:-400}, 500, moveFirstSlide);
+// $(function(){
+// 	var carouselList = $("#carousel ul");
+// 	carouselList.animate({marginLeft:-400}, 500, moveFirstSlide);
+// 	setInterval(changeSlide, 3000);
 
-function moveFirstSlide() {
-	var firstItem = carouselList.find("li:first");
-	var lastItem = carouselList.find("li:last");
-	lastItem.after(firstItem)
-	carouselList.css({marginLeft:0});
-};
+// 	function moveFirstSlide() {
+// 		var firstItem = carouselList.find("li:first");
+// 		var lastItem = carouselList.find("li:last");
+// 		lastItem.after(firstItem)
+// 		carouselList.css({marginLeft:0});
+// 	};
+// });
+
+$(function(){
+	var carouselList = $("#carousel ul");
+	console.log(carouselList);
+	function setInterval(changeSlide, 3000) {
+	function changeSlide() { 
+	carouselList.animate({marginLeft:-400}, 500, moveFirstSlide);
+	}
+	function moveFirstSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem)
+		carouselList.css({marginLeft:0});
+	};
+}
 });
 
