4cd50d6a30709b459d3f8b3b8899389810072d1a
Mike <mikikoman@gamil.com>
2016-07-14 00:10:22 +0200

    slider finished

--- script.js
+++ script.js
@@ -1,72 +1,47 @@
 
 $(function(){
-
     var carouselList = $("#carousel ul");
     var dots = $(".dots");
     var prev = $(".glyphicon-chevron-left");
     var next = $(".glyphicon-chevron-right");
-
     var current = 0,
-            offset = -400;
+        offset = -400;
 
     function initDots() {
-
         var k = carouselList.find('li').length-1;
-
         for (var c = 0; c < k ; c++) {
 
-            var element = $('<button class="btn-primary">o</button>');
+            var element = $('<button>o</button>');
             element.on('click', changeSlide.bind(this, c));
 
 
             dots.append(element)
-
         }
-
     }
     
     prev.click( function () {
-
         current--;
         if (current < 0){
             current = carouselList.find('li').length-2;
         }
-
         carouselList.css({marginLeft: current*offset });
-
     }) ;
-    next.click( function () {
 
+    next.click( function () {
         current++;
         if (current == carouselList.find('li').length-1){
             current = 0;
         }
-
         carouselList.css({marginLeft: current*offset });
-
     }) ;
-
-    // function moveFirstSlide() {
-
-    //      current = 0;
-
-    //      var firstItem = carouselList.find("li:first");
-    //      var lastItem = carouselList.find("li:last");
-    //      lastItem.after(firstItem)
-
-    //     carouselList.css({marginLeft:-40});
-    // }
 
     function changeSlide(selected) {
 
         if (selected != undefined) {
-
             current = selected;
-
-        } else {
-
+        } 
+        else {
             current++;
-
         }
 
         if (current >= carouselList.find('li').length - 1) {
@@ -75,46 +50,15 @@ $(function(){
 
               current = 0;
               carouselList.css({marginLeft: current*offset });
-
             });
 
-        } else {
+        }
+        else {
 
             carouselList.animate({marginLeft: current*offset }, 500);
-
         }
     }
-
-
-
-            current = 0;
-    console.log(carouselList);
-
-    
-    
-
     initDots();
 
-    var current = 0;
-
-    // function changeSlide() {
-
-    //     current++;
-
-    //     if (current >= carouselList.children().length-1) {
-
-    //         carouselList.animate({marginLeft: '-=400' }, 500, moveFirstSlide);
-
-    //     } else {
-
-    //         carouselList.animate({marginLeft: '-=400' }, 500);
-
-    //     }
-
-    // }
     setInterval(changeSlide, 2000)
-
-
-
-
 });
